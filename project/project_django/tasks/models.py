32e273fd03d5b307cba179549b447ab82f87dc8e
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-05-27 12:23:33 -0500

    Eliminación carpeta tasks para hacerla de cero nuevamente

--- project/project_django/tasks/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models # type: ignore
-
-# Create your models here.
-class Task(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.TextField()
