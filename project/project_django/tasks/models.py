91827322d1588ec6b9ce02ea2c13e232254e49ef
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-05-27 12:18:35 -0500

    Creación clase Task

--- project/project_django/tasks/models.py
+++ project/project_django/tasks/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
